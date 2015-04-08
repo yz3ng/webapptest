3491f8c6274fa733ca5c7671d8fbaa47d3b9b994
Yili Zeng <yilizeng@gmail.com>
2015-04-07 21:37:40 -0400

    Update PhoneBook.java

--- src/com/proquest/interview/phonebook/PhoneBook.java
+++ src/com/proquest/interview/phonebook/PhoneBook.java
@@ -1,6 +1,33 @@
 package com.proquest.interview.phonebook;
 
 public interface PhoneBook {
+	/**
+     * Find a person by first name and last name
+     * 
+     * @param firstName, lastName
+     * @return Person
+     */
 	public Person findPerson(String firstName, String lastName);
+	
+	/**
+     * Add a new person
+     * 
+     * @param newPerson
+     */
 	public void addPerson(Person newPerson);
+	
+	/**
+     * Remove a person from the phone book
+     * 
+     * @param newPerson
+     */
+	public void removePerson(Person person);
+	
+	/**
+     * Display the phone book
+     * 
+     * @return String
+     */
+	public String DisplayPhoneBook();
+	
 }
