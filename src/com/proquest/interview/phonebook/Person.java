44a396b90432edd5d2c0f238928d47f99a1c53ea
Yili Zeng <yilizeng@gmail.com>
2015-04-07 21:34:55 -0400

    Update Person.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -1,7 +1,47 @@
 package com.proquest.interview.phonebook;
 
 public class Person {
-	public String name;
+	public String firstName;
+	public String lastName;
 	public String phoneNumber;
 	public String address;
+	
+	public Person(String firstName, String lastName, String phoneNumber, String address){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+	
+	public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
